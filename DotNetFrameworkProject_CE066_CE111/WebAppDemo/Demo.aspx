be5517f4a3695ac114d6acfefd44ab39d32db19e
RohitPrajapati9593 <rohitprajapati.bbit@gmail.com>
2022-10-17 07:17:48 +0530

    Initial commit

---
+++ DotNetFrameworkProject_CE066_CE111/WebAppDemo/Demo.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Demo.aspx.cs" Inherits="WebAppDemo.Demo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Note_taking_title_Lbl" runat="server" Text="Note Taking"></asp:Label>
+            <br />
+            <br />
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Id_Lbl" runat="server" Text="Id"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="Id_Txt" runat="server"></asp:TextBox>
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:Label ID="Note_Title_Lbl" runat="server" Text="Note Title"></asp:Label>
+            &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="Note_title_Txt" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label1" runat="server" Text=" Note Message"></asp:Label>
+            &nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="Note_message_Txt" runat="server" Height="127px" Width="314px"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="INSERT_Btn" runat="server" OnClick="INSERT_Btn_Click" Text="INSERT" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="UPDATE_Btn" runat="server" OnClick="UPDATE_Btn_Click" Text="UPDATE" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="DELETE_Btn" runat="server" OnClick="DELETE_Btn_Click" Text="DELETE" />
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="SHOW_Btn" runat="server" OnClick="SHOW_Btn_Click" Text="SHOW" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#2461BF" />
+                <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#EFF3FB" />
+                <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                <SortedDescendingHeaderStyle BackColor="#4870BE" />
+            </asp:GridView>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ DotNetFrameworkProject_CE066_CE111/WebAppDemo/Demo.aspx.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppDemo
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+
+        protected void ShowStudent()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["notelistConnection"].ConnectionString;
+            try
+            {
+                using (con)
+                {
+                    string command = "Select * from NoteList";
+                    SqlCommand cmd = new SqlCommand(command, con);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    GridView1.DataSource = rdr;
+                    GridView1.DataBind();
+                    rdr.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: " + ex.Message);
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void INSERT_Btn_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["notelistConnection"].ConnectionString;
+            try
+            {
+                using (con)
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO NoteList VALUES (@Note_title,@Note_content)", con);
+
+
+                    cmd.Parameters.AddWithValue("@Id", int.Parse(Id_Txt.Text));
+
+                    cmd.Parameters.AddWithValue("@Note_title", Note_title_Txt.Text);
+                    cmd.Parameters.AddWithValue("@Note_content", Note_message_Txt.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    //   MessageBox.Show("Data stored successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("errors:" + ex.Message);
+            }
+
+
+            //         cmd.Parameters.AddWithValue("@Note_title", int.Parse(Note_title_Txt.Text));
+
+            //          cmd.Parameters.AddWithValue("@Note_content", int.Parse(Note_message_Txt.Text));
+
+
+
+        }
+
+        protected void UPDATE_Btn_Click(object sender, EventArgs e)
+        {
+
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["notelistConnection"].ConnectionString;
+            try
+            {
+                using (con)
+                {
+                    SqlCommand cmd = new SqlCommand("UPDATE NoteList SET Note_title=@Note_title,Note_content=@Note_content WHERE Id=@Id", con);
+
+
+                    cmd.Parameters.AddWithValue("@Id", int.Parse(Id_Txt.Text));
+
+                    cmd.Parameters.AddWithValue("@Note_title", Note_title_Txt.Text);
+                    cmd.Parameters.AddWithValue("@Note_content", Note_message_Txt.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    //   MessageBox.Show("Data stored successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("errors:" + ex.Message);
+            }
+
+
+        }
+
+        protected void DELETE_Btn_Click(object sender, EventArgs e)
+        {
+
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["notelistConnection"].ConnectionString;
+            try
+            {
+                using (con)
+                {
+                    SqlCommand cmd = new SqlCommand("DELETE NoteList   WHERE Id=@Id", con);
+
+
+                    cmd.Parameters.AddWithValue("@Id", int.Parse(Id_Txt.Text));
+
+                    cmd.Parameters.AddWithValue("@Note_title", Note_title_Txt.Text);
+                    cmd.Parameters.AddWithValue("@Note_content", Note_message_Txt.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    //   MessageBox.Show("Data stored successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("errors:" + ex.Message);
+            }
+
+
+
+        }
+
+        protected void SHOW_Btn_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["notelistConnection"].ConnectionString;
+            try
+            {
+                using (con)
+                {
+                    string command = "Select * from NoteList";
+                    SqlCommand cmd = new SqlCommand(command, con);
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    GridView1.DataSource = rdr;
+                    GridView1.DataBind();
+                    //rdr.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: " + ex.Message);
+            }
+            /*
+             SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["notelistConnection"].ConnectionString;
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM NoteList ", con);
+            SqlDataAdapter da=new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            */
+        }
+    }
+}
